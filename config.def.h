1539c6e489d2f73851b1a155bf7f9521b8dce36f
Fikri Rahmat Nurhidayat <fikrirnurhidayat@gmail.com>
2023-12-03 03:13:16 +0700

    feat: update configuration

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka Term:size=12:antialias=true:autohint=true";
+static char *font = "IBM Plex Mono:size=12:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
